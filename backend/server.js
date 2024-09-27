4f598f0d24f18845b1ab9921f972a5fad7ba2d78
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2024-09-28 00:51:09 +0530

    auth route setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import connectToDatabase from "./db/connectToDatabase.js";
 
+import authRoutes from './routes/auth.routes.js'
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || 5000 ;
 
 app.use(express.json());                    // to parse incomming requests with JSON payloads ( from req.body );
 app.use(cookieParser());
+app.use("/api/auth" , authRoutes );
 
 app.get("/" , (req , res )=>{
     res.send("home page is here");
