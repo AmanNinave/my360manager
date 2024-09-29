6cab91580d921d24ad8c04b2d82be56203d6b21c
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2024-09-29 18:12:02 +0530

    serving static file with express

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import path from 'path'
 
 import connectToDatabase from "./db/connectToDatabase.js";
 
@@ -13,6 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000 ;
 
+const __dirname = path.resolve();
+
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
@@ -23,8 +26,10 @@ app.use(cookieParser());
 app.use("/api/auth" , authRoutes );
 app.use("/api/finance" , financeRoutes)
 
-app.get("/", (req , res)=>{
-    res.send("hello");
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*" , ( req , res ) => {
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"));
 })
 
 connectToDatabase().then(()=>{
