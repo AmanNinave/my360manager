16d2b921ce0ee2ce36fcdd766a8b189179507bbe
Aman Ninave <amanninave@192.168.1.6>
2024-09-29 16:07:35 +0530

    index file changed for vercel

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
-// import path from 'path'
+import path from 'path'
 
 import connectToDatabase from "./db/connectToDatabase.js";
 
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000 ;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -26,11 +26,11 @@ app.use(cookieParser());
 app.use("/api/auth" , authRoutes );
 app.use("/api/finance" , financeRoutes)
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-// app.get("*" , ( req , res ) => {
-//     res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"));
-// })
+app.get("*" , ( req , res ) => {
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"));
+})
 
 
 connectToDatabase().then(()=>{
