b4e49ee5199f8f3668e0dc5502509d23073bf59c
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2024-09-29 17:39:48 +0530

    config changed for backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
-import path from 'path'
 
 import connectToDatabase from "./db/connectToDatabase.js";
 
@@ -14,8 +13,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000 ;
 
-const __dirname = path.resolve();
-
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
@@ -26,12 +23,9 @@ app.use(cookieParser());
 app.use("/api/auth" , authRoutes );
 app.use("/api/finance" , financeRoutes)
 
-app.use(express.static(path.join(__dirname, "/frontend")))
-
-app.get("*" , ( req , res ) => {
-    res.sendFile(path.join(__dirname , "frontend" , "index.html"));
+app.get("/", (req , res)=>{
+    res.send("hello");
 })
-
 
 connectToDatabase().then(()=>{
     app.listen( PORT , ()=>{
